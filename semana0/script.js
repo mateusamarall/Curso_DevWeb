ce304efe49f406159d6cebf15d7e4e6a4e304ca1
mateusamarall <mateus.amaral018@gmail.com>
2020-05-11 17:33:10 -0300

    tipos de variaveis

---
+++ semana0/script.js
@@ -0,0 +1,18 @@
+//string
+var nome = "mateus";
+//int
+var idade = 18;
+//float
+var altura = 1.75;
+//booleans
+var sexo_masculino = true;
+var sexo_feminino = false;
+//lista
+var lista = [nome, idade, altura, sexo_masculino, sexo_feminino];
+//objetos
+var objAluno = {
+  nome: lista[0],
+  idade: lista[1],
+  sexo_masculino: lista[3],
+};
+console.log(objAluno);
