ceb887cee10cf135c9fe0a1e45f6d036b0bd9213
mateusamarall <mateus.amaral018@gmail.com>
2020-06-27 19:58:10 -0300

    adicionada novas rotas e entendendo sobre parametros

--- NodeJS/routes.js
+++ NodeJS/routes.js
@@ -1,8 +1,32 @@
 const express = require("express");
 
 const routes = express.Router();
-
+const users = {};
 routes.get("/", (req, res) => {
-  return res.send("hello world usando o nodemon");
+  //listagem
+  return res.json({ users });
+});
+routes.post("/", (req, res) => {
+  //criacao
+  const { nome, cidade, idade } = req.body;
+
+  users[nome] = { cidade, idade, nome };
+
+  return res.json({ message: "usuário criado" });
+});
+
+routes.put("/:nome", (req, res) => {
+  //edicao
+  const { nome } = req.params;
+  const { cidade, idade } = req.body;
+
+  users[nome] = { cidade, idade, nome };
+  return res.json({ message: "usuario editado" });
+});
+
+routes.delete("/:nome", (req, res) => {
+  const { nome } = req.params;
+  delete users[nome];
+  res.json({ messsage: `usuário ${nome} foi deletado com sucesso.` });
 });
 module.exports = routes;
