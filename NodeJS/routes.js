330e65c049c6e8ad20992aa1e1819fb70ae2d863
mateusamarall <mateus.amaral018@gmail.com>
2020-07-13 19:24:11 -0300

    adicionado mongoose para fazer a conexão com o banco de dados

--- NodeJS/routes.js
+++ NodeJS/routes.js
@@ -6,27 +6,5 @@ routes.get("/", (req, res) => {
   //listagem
   return res.json({ users });
 });
-routes.post("/", (req, res) => {
-  //criacao
-  const { nome, cidade, idade } = req.body;
 
-  users[nome] = { cidade, idade, nome };
-
-  return res.json({ message: "usuário criado" });
-});
-
-routes.put("/:nome", (req, res) => {
-  //edicao
-  const { nome } = req.params;
-  const { cidade, idade } = req.body;
-
-  users[nome] = { cidade, idade, nome };
-  return res.json({ message: "usuario editado" });
-});
-
-routes.delete("/:nome", (req, res) => {
-  const { nome } = req.params;
-  delete users[nome];
-  res.json({ messsage: `usuário ${nome} foi deletado com sucesso.` });
-});
 module.exports = routes;
