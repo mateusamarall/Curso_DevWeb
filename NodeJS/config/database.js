cabc108b724ba0bc1cf3c622a3c4cc79a325fbfc
mateusamarall <mateus.amaral018@gmail.com>
2020-07-13 19:23:17 -0300

    conexão com o banco de dados MongoDB

---
+++ NodeJS/config/database.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  "mongodb+srv://mateusparajogoss@gmail.com:34551535@cursodevweb.gplu8.gcp.mongodb.net/CursoDevWeb?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
